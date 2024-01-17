a7839652bac7ded57010b57befcfdfb62ac694de
beefreguglia <beefreguglia@gmail.com>
2024-01-17 14:36:46 -0300

    feature: create tests e2e to test cart

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -6,6 +6,7 @@ import { useSearchParams } from 'next/navigation'
 export default function SearchLoading() {
   const searchParams = useSearchParams()
   const query = searchParams.get('q')
+
   return (
     <div className="flex flex-col gap-4">
       <p className="text-sm">
